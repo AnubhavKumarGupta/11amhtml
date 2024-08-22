7fde6d25b0b19363edfd3f15d6f186f90646737d
prasad-bigdp <bigdp585@gmail.com>
2024-08-22 12:53:14 +0530

    update

--- JS/code/DataTypes/script.js
+++ JS/code/DataTypes/script.js
@@ -29,7 +29,71 @@ console.log(Math.round(Math.random() * 10000)) // it will give random value betw
 
 
 
-// numbers have some default methods 
+    // strings
+let str = "JS is so easy"
+console.log(str, typeof str)
+let str1 = "time is 12 o' clock"
+let str2 = 'the gandhi said "good night"'
+// backtick is used for strings. it is called template literal. it is used to assign dynamic values inside the string without breaking it
+let p = 5, q = 10;
+console.log(" the sum of " + p + " and " + q + " is " + (p + q));
+console.log(`the sum of ${p} and ${q} is ${p+q}`)
+let pagename = "fruits";
+let url = `https://www.amazon.in/${pagename}`
+
+// take cost=10, items=5; print in console total value without breaking string
+let cost = 10, items = 5;
+console.log(` the total cost is ${cost * items}`)
+
+// strings have some default functions
+// Every string is nothing but an array of characters
+let s="prasad"
+console.log(s[0] + s[2])
+console.log(s.length) //6 - no.of characters
+console.log(s.concat(" raj")) //prasad raj
+console.log(s.toUpperCase(),s) // PRASAD
+// strings are immutable- u cannot change
+console.log(s.toLowerCase()) //prasad
+console.log(s.startsWith('pr')) //true
+let na = "adha sharma"
+console.log(na.endsWith('sharma')) //true
+console.log(s.indexOf('a')) // first occurence value index
+console.log(s.lastIndexOf('a')) //4
+console.log(s.charAt('0')) //p
+console.log("    prasad  ".trim()) // remove space from last and first
+console.log("js is so easy".split("")) // split is used to split the string based on delimiter and return an array
+console.log(s.replaceAll('a', 'p'))
+console.log("prasad".slice(2))//asad
+console.log("prasad".slice(1, 5)) //rasa
+
+// boolean
+let isGoodBoy = true
+console.log(1 + isGoodBoy)
+
+// undefined and null
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ JS/code/DataTypes/script2.js
@@ -0,0 +1,9 @@
+console.log(a)
+var a = 55;
+console.log(b)
+var b = 65;
+console.log(x)
+let x = 25;
+console.log(a)
+a++;
+console.log(a)
